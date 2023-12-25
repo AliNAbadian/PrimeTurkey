791e2e7243d8edd99ec24303e704c99ff2b8b81e
Ali N. Abadian <nagshrizali@gmail.com>
2023-12-25 18:15:47 +0330

    Test

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      screens: {
+        lg: "1250px",
+      },
+    },
+  },
+  plugins: [],
+};
